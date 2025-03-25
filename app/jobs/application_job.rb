4fb96eac136e506cf07aaedb775d396750d77415
emersoftware <87254036+emersoftware@users.noreply.github.com>
2025-03-25 17:36:01 -0300

    testing: profile memory (ruby version, hash syntax)

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,32 @@
-class ApplicationJob < ActiveJob::Base
-end
+module PushNotifications
+    module Promoters
+      class SendGroupLoanApplicationPassedToReviewJob < ApplicationJob
+        def perform
+
+          message_params
+        end
+  
+        private
+  
+        def message_params
+          {
+            data: {
+              notifee: {
+                id: '2025blar2025blar2025',
+                title:,
+                body:,
+                android: android_params
+              }.to_json
+            },
+            android: {
+              priority: 'high'
+            }
+          }
+        end
+  
+        def android_params
+          { some_android_key: '2025blar2025' }
+        end
+      end
+    end
+  end
