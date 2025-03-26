3ad36e508c5618b226ad9627941ebe68f7edee63
emersoftware <87254036+emersoftware@users.noreply.github.com>
2025-03-26 18:01:26 -0300

    feat: test (add local variables)

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,30 +2,32 @@ module PushNotifications
     module Promoters
       class SendGroupLoanApplicationPassedToReviewJob < ApplicationJob
         def perform
-
           message_params
         end
   
         private
   
         def message_params
+          title = "Default Title"
+          body  = "Default Body"
+  
           {
             data: {
               notifee: {
-                id: '2025blar2025blar2025',
+                id: "group_loan_application_passed_to_review",
                 title:,
                 body:,
                 android: android_params
               }.to_json
             },
             android: {
-              priority: 'high'
+              priority: "high"
             }
           }
         end
   
         def android_params
-          { some_android_key: '2025blar2025' }
+          { some_android_key: "some_android_value" }
         end
       end
     end
